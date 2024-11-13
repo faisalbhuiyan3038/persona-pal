badc6e7e9ba2545ec0530d77f8a814df5b1d8692
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-11-13 19:18:51 +0600

    add a responsive sidebar with tweaks for mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,11 +3,12 @@
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
 
-import { Menu, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import { Poppins } from 'next/font/google';
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const font = Poppins({
   weight: "600",
@@ -17,9 +18,9 @@ const font = Poppins({
 export const Navbar = () => {
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4
-     border-b border-primary/10 bg-secondary">
+     border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
           <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
             font.className
