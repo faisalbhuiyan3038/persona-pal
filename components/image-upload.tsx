832c844a6bd506c5b7598cf75cf6741fdde1ac19
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-11-15 18:28:26 +0600

    finish companion creation form with values test

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -28,15 +28,17 @@ export const ImageUpload = ({
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
-      <CldUploadButton options={{
-        maxFiles: 1
-      }}
+      <CldUploadButton
+        onSuccess={(result: any) => onChange(result.info.secure_url)}
+        options={{
+          maxFiles: 1
+        }}
         uploadPreset="xuy58vw5"
       >
         <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg
         hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
           <div className="relative h-40 w-40">
-            <Image fill alt="Upload" src="/placeholder.svg" className="rounded-lg object-cover" />
+            <Image fill alt="Upload" src={value || "/placeholder.svg"} className="rounded-lg object-cover" />
           </div>
         </div>
       </CldUploadButton>
