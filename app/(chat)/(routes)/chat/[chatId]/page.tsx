b0aeab1b7a322577b42b8b7622c86dace8181140
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-11-17 23:35:27 +0600

    create chat page and design header

---
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -0,0 +1,50 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { ChatClient } from "./components/client";
+
+interface ChatIdPageProps {
+  params: {
+    ChatId: string;
+  }
+}
+
+const ChatIdPage = async ({
+  params
+}: ChatIdPageProps) => {
+
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId) return redirectToSignIn();
+
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.ChatId,
+    },
+    include: {
+      messages: {
+        orderBy: {
+          createdAt: "asc",
+        },
+        where: {
+          userId,
+        }
+      },
+      _count: {
+        select: {
+          messages: true
+        }
+      }
+    }
+  });
+
+  if (!companion) {
+    return redirect("/");
+  }
+
+  return (
+    <ChatClient companion={companion} />
+  );
+}
+
+export default ChatIdPage;
