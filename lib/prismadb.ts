db5ccb4adf09cbe472c39886ec09e01a23146290
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-11-23 18:10:00 +0600

    fix prisma declaration

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 //to fix prisma not available as global object
 declare global {
-  let prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 };
 
 //to fix next hot reloading causing issues with prisma init
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb;
+const prismadb = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') global.prisma = prismadb;
 
 export default prismadb;
